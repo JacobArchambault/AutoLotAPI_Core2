23e3d436735aa72cb954830c213baa33136a2061
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-27 05:09:06 -0400

    Add constructor to InventoryController.

---
+++ AutoLotMVC_Core2/Controllers/InventoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AutoLotDAL_Core2.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace AutoLotMVC_Core2.Controllers
+{
+    public class InventoryController : Controller
+    {
+        public readonly string _baseUrl;
+        public InventoryController(IConfiguration configuration)
+        {
+            _baseUrl = configuration.GetSection("ServiceAddress").Value;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
