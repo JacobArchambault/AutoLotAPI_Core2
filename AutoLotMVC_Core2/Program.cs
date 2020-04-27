40df70c8c1622ccefa5dbae5cb526cd0461a71d6
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-27 04:32:14 -0400

    Add AutoLotMVC_Core2 project to solution, and set both it and AutoLotAPI as startup projects.

---
+++ AutoLotMVC_Core2/Program.cs
@@ -0,0 +1,31 @@
+using AutoLotDAL_Core2.DataInitialization;
+using AutoLotDAL_Core2.EF;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AutoLotMVC_Core2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webHost = CreateHostBuilder(args).Build();
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<AutoLotContext>();
+                MyDataInitializer.RecreateDatabase(context);
+                MyDataInitializer.InitializeData(context);
+            }
+            webHost.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
